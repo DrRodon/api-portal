69e2d5bc09df9ec48c4129b5f60cbc4f36df59b6
DrRodon <krzysztof.rodon@gmail.com>
2026-01-08 23:07:04 +0100

    Add initial HTML, CSS, and JavaScript files for the health portal application

---
+++ script.js
@@ -0,0 +1,11 @@
+﻿const tiles = document.querySelectorAll(".tile");
+
+const revealTiles = () => {
+  tiles.forEach((tile, index) => {
+    setTimeout(() => {
+      tile.classList.add("is-visible");
+    }, 120 * index);
+  });
+};
+
+window.addEventListener("load", revealTiles);
